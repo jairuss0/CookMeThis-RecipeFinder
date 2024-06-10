c99f99980609123369ae7887f8bd5813eb0f3f8c
jairuss0 <elijahjairusc@gmail.com>
2024-06-10 11:42:39 +0800

    responses for list added

--- js/findRecipe.js
+++ js/findRecipe.js
@@ -1,22 +1,28 @@
 const recipeInput = document.getElementById("recipeInput");
 const filterBtn = document.querySelector(".filter-btn");
 const recipesParentDiv = document.querySelector(".recipes-result");
+const arrowIcon = document.getElementById("arrow");
+
+const mealBtns = document.querySelector(".meal-div");
+const areaBtns = document.querySelector(".area-div");
+
 
 document.addEventListener("DOMContentLoaded", async () => {
-    initialMeal();
-    
+  initialMeal();
+  getResponseByMeals(); 
+  getResponseByArea();
 });
 
 // create initial meal response
 async function initialMeal() {
   try {
     const ingredientResponse = await fetchApiResponseByIngredient("egg");
-    console.log(ingredientResponse);
     renderIngredients(ingredientResponse);
   } catch (error) {
     console.error(error);
   }
 }
+
 // fetch reponse via main ingredient
 async function fetchApiResponseByIngredient(ingredient) {
   try {
@@ -36,10 +42,10 @@ async function getResponseByIngredient() {
   const ingredientInput = recipeInput.value.toLowerCase();
   if (ingredientInput) {
     try {
-      const response = await fetchApiResponseByIngredient(ingredientInput);
+      const responseIngredient = await fetchApiResponseByIngredient(ingredientInput);
       // pass the response onto the render function
       recipeInput.value = "";
-      renderIngredients(response);
+      renderIngredients(responseIngredient);
     } catch (error) {
       console.error(error);
       // create a function to render error on webpage
@@ -47,16 +53,87 @@ async function getResponseByIngredient() {
   }
 }
 
+async function fetchApiResponseByMeals() {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/list.php?c=list`
+    );
+    if (!response.ok) {
+      console.log("Error: response Failed");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+// FILTER REPONSES BY CATEGORY - meal, AREA
+// get the category meal response
+async function getResponseByMeals() {
+  try {
+    const responseByMealBtns = await fetchApiResponseByMeals();
+
+    renderMealBtns(responseByMealBtns);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchApiResponseByAreas() {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/list.php?a=list`
+    );
+    if (!response.ok) {
+      console.log("Error: response Failed");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+// FILTER REPONSES BY CATEGORY - meal, AREA
+// get the category meal response
+async function getResponseByArea() {
+  try {
+    const responseByAreaBtns = await fetchApiResponseByAreas();
+    renderAreaBtns(responseByAreaBtns);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+// render meal category buttons
+function renderMealBtns(response){
+  console.log(response);
+  response.meals.forEach((meal) => {
+    const button = document.createElement('button');
+    button.classList.add("meal-filter-btn");
+    button.textContent = meal.strCategory;
+    mealBtns.appendChild(button);
+  });
+  
+}
+// render area category buttons
+function renderAreaBtns(response){
+  console.log(response);
+  response.meals.forEach((meal) => {
+    const button = document.createElement('button');
+    button.classList.add("area-filter-btn");
+    button.textContent = meal.strArea;
+    areaBtns.appendChild(button);
+  });
+  
+}
+
 // render response via main ingredient
 function renderIngredients(ingredientResponse) {
-
   console.log(ingredientResponse);
-
   let recipeCard = "";
   if (ingredientResponse) {
     ingredientResponse.meals.forEach((meal) => {
       recipeCard += `<div class="col-lg-4 col-md-6">
-                                <div class="card meal-card" style="width: 100%;">
+                                <div class="card meal-card" style="min-width: 100%;">
                                     <img src="${meal.strMealThumb}" class="card-img-top" id="meal-img" alt="meal">
                                     <div class="card-body">
                                         <p class="meal-name">${meal.strMeal}</p>
@@ -67,7 +144,6 @@ function renderIngredients(ingredientResponse) {
     });
     recipesParentDiv.innerHTML = recipeCard;
   }
-  
 }
 
 // if user keyup pressed an enter
@@ -75,5 +151,28 @@ document.onkeyup = (e) => {
   if (e.key == "Enter" && recipeInput.value.length != 0) {
     e.preventDefault();
     getResponseByIngredient();
+    
   }
 };
+
+filterBtn.addEventListener('click', () => {
+  arrowIcon.classList.toggle("fa-arrow-up");
+  arrowIcon.classList.toggle("fa-arrow-down");
+  
+});
+
+
+/*
+
+  Major
+
+  1. get the user clicked 'see recipe' meal Id and display it on another html page 
+  2. create local storage for user favorites, new interface and repeat no.1 logic
+
+
+  minor
+  1. implement load more
+  2. implement loading animation while fetching api calls
+
+
+*/
